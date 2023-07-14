1c7c9c68c126406ef608e83012ccafe22ac75213
Anton Bauer <bauer_av+github@proton.me>
2023-07-14 20:43:16 +0200

    Use hierarchical config

---
+++ lua/config/omnisharp.lua
@@ -0,0 +1,2 @@
+vim.g.OmniSharp_server_use_net6 = 1
+vim.g.OmniSharp_translate_cygwin_wsl = 1
---
+++ lua/plugins.lua
@@ -0,0 +1,9 @@
+require('config.omnisharp')
+
+local Plug = vim.fn['plug#']
+
+vim.call('plug#begin')
+
+Plug 'OmniSharp/omnisharp-vim'
+
+vim.call('plug#end')
