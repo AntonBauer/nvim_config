e7ffcb3340303e82ace614b94212571a46f763df
Anton Bauer <bauer_av+github@proton.me>
2023-07-18 21:31:10 +0200

    Use kickstart nvim for initial setup

--- lua/config/omnisharp.lua
+++
@@ -1,2 +0,0 @@
-vim.g.OmniSharp_server_use_net6 = 1
-vim.g.OmniSharp_translate_cygwin_wsl = 1
--- lua/plugins.lua
+++
@@ -1,9 +0,0 @@
-require('config.omnisharp')
-
-local Plug = vim.fn['plug#']
-
-vim.call('plug#begin')
-
-Plug 'OmniSharp/omnisharp-vim'
-
-vim.call('plug#end')
