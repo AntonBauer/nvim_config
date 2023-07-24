d8d9d08380d701fc542c2b4506990c2d13c45c5f
Anton Bauer <bauer_av+github@proton.me>
2023-07-24 20:43:16 +0200

    Implemented working OmniSharp config

---
+++ lua/config/omnisharp.lua
@@ -0,0 +1,8 @@
+-- OmniSharp config
+vim.g.OmniSharp_server_stdio = 1
+vim.g.OmniSharp_server_use_net6 = 1
+vim.g.OmniSharp_autoselect_existing_sln = 1
+
+-- ALE config
+vim.g.ale_linters = {cs = 'OmniSharp'}
+
