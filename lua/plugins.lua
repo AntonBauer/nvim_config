cc769a2e9ca34266591c729cd078bc65f4a3bebc
Anton Bauer <bauer_av+github@proton.me>
2023-08-05 18:04:11 +0200

    extract lsp plugins installation

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -7,25 +7,7 @@ return {
   -- "gc" to comment visual regions/lines
   { 'numToStr/Comment.nvim', opts = {} },
 
-  -- NOTE: This is where your plugins related to LSP can be installed.
-  --  The configuration is done below. Search for lspconfig to find it below.
-  {
-    -- LSP Configuration & Plugins
-    'neovim/nvim-lspconfig',
-    dependencies = {
-      -- Automatically install LSPs to stdpath for neovim
-      { 'williamboman/mason.nvim', config = true },
-      'williamboman/mason-lspconfig.nvim',
-
-      -- Useful status updates for LSP
-      -- NOTE: `opts = {}` is the same as calling `require('fidget').setup({})`
-      { 'j-hui/fidget.nvim', tag = 'legacy', opts = {} },
-
-      -- Additional lua configuration, makes nvim stuff amazing!
-      'folke/neodev.nvim',
-    },
-  },
-
+  require 'plugins.lsp',
   require 'plugins.cmp',
   require 'plugins.gitsigns',
   require 'plugins.theme',
