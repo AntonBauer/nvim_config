bbbb09e6fef4a51a03844486ecfe138599d333d9
Anton Bauer <bauer_av@proton.me>
2023-08-22 20:16:14 +0200

    [Treesitter] ensure speciifc languages parsers are installed

--- lua/config/treesitter.lua
+++ lua/config/treesitter.lua
@@ -2,10 +2,26 @@
 -- See `:help nvim-treesitter`
 require('nvim-treesitter.configs').setup {
   -- Add languages to be installed here that you want installed for treesitter
-  ensure_installed = { 'c', 'cpp', 'go', 'lua', 'python', 'rust', 'tsx', 'typescript', 'vimdoc', 'vim' },
+  ensure_installed = {
+    'lua',
+    'vimdoc',
+    'vim',
+    'c_sharp',
+    'bash',
+    'diff',
+    'dockerfile',
+    'git_config',
+    'gitignore',
+    'json',
+    'regex',
+    'sql',
+    'terraform',
+    'xml',
+    'yaml'
+  },
 
   -- Autoinstall languages that are not installed. Defaults to false (but you can change for yourself!)
-  auto_install = false,
+  auto_install = true,
 
   highlight = { enable = true },
   indent = { enable = true },
