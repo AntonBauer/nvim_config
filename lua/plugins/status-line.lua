7d7f05b323c51521ea6867d6a89217fc185c7816
Anton Bauer <bauer_av@proton.me>
2023-09-12 21:41:26 +0200

    update plugins, add icons support

--- lua/plugins/status-line.lua
+++ lua/plugins/status-line.lua
@@ -1,13 +1,15 @@
 return {
-  -- Set lualine as statusline
-  'nvim-lualine/lualine.nvim',
-  -- See `:help lualine.txt`
-  opts = {
-    options = {
-      icons_enabled = true,
-      theme = 'palenight',
-      component_separators = '|',
-      section_separators = '',
-    },
+  {
+    'nvim-lualine/lualine.nvim',
+    -- See `:help lualine.txt`
+    opts = {
+      options = {
+        icons_enabled = true,
+        theme = 'horizon',
+        component_separators = '|',
+        section_separators = '',
+      },
+    }
   },
+  'nvim-tree/nvim-web-devicons'
 }
